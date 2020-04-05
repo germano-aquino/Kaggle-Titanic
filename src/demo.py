7d807ad8204b3805156b7b8999f2a118e7c6bd24
Germano Aquino <germano.r.aquino@gmail.com>
2020-04-05 01:04:50 -0300

    atualizacao demo.py

--- src/demo.py
+++ src/demo.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 
 from cleaning import cleanTitanic
@@ -20,11 +21,11 @@ catClass = ['Pclass', 'Sex', 'Embarked', 'Age']
 alive = train[train['Survived'] == 1]
 deads = train[train['Survived'] == 0]
 
-sns.boxplot(data=train, x="Survived", y='Fare')
-plt.show()
-print(train.columns)
-#alive.Parch.plot.hist(alpha=.5, bins=18, color='red')
-#deads.Parch.plot.hist(alpha=.5, bins=18, color='blue')
+#sns.boxplot(data=train, x="Survived", y='Fare')
+# plt.show()
+# print(train.columns)
+#alive.Age.plot.hist(alpha=.5, bins=50, color='red')
+#deads.Age.plot.hist(alpha=.5, bins=50, color='blue')
 #plt.legend(['alive', 'dead'])
 # plt.show()
 
@@ -32,22 +33,30 @@ testCleaned = cleanTitanic(test, dropCols, catClass, ['Fare'])
 trainCleaned = cleanTitanic(train, dropCols, catClass=catClass)
 
 X, y = XandY(trainCleaned, dummyCols=catClass)
-print(X.columns)
+print(X.Fare.head())
 testCleaned['Survived'] = [None]*testCleaned.shape[0]
 Xresult, yresult = XandY(testCleaned, dummyCols=catClass)
 
 Xtrain, Xtest, ytrain, ytest = train_test_split(
     X, y, test_size=.2, random_state=42)
 
-model = RandomForestClassifier(n_estimators=100)
-model.fit(Xtrain, ytrain)
+rf = RandomForestClassifier(random_state=42)
+params_rf = {'n_estimators': [25, 30, 35], 'max_features': [
+    'log2', 'auto', 'sqrt'], 'min_samples_leaf': [3, 4, 5]}
+grid_rf = GridSearchCV(estimator=rf, param_grid=params_rf,
+                       cv=5, scoring='accuracy', n_jobs=-1)
+grid_rf.fit(Xtrain, ytrain)
 
+model = grid_rf.best_estimator_
+print(grid_rf.best_score_)
+print(grid_rf.best_params_)
 ypred = model.predict(Xtest)
-print("Accuracy with non seen data: ", accuracy_score(ypred, ytest))
-scores = cross_val_score(model, X, y, cv=5)
 
-print("Score with cross validation: ", scores.mean())
+print("Accuracy with non seen data: ", accuracy_score(ypred, ytest))
+#scores = cross_val_score(model, X, y, cv=5)
+
+#print("Score with cross validation: ", scores.mean())
 
 test['Survived'] = model.predict(Xresult)
 
-test[['PassengerId', "Survived"]].to_csv('kaggle_submission3.csv', index=False)
+test[['PassengerId', "Survived"]].to_csv('kaggle_submission4.csv', index=False)
