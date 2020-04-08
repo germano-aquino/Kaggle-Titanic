d451d27382e82aba8d184677f97c13b1b1ed091e
Germano Aquino <germano.r.aquino@gmail.com>
2020-04-08 13:53:16 -0300

    Name pre processing

--- src/cleaning.py
+++ src/cleaning.py
@@ -2,6 +2,31 @@ import pandas as pd
 
 ages = [-1, 0, 5, 12, 18, 30, 60, 100]
 labels = ['missing', 'infant', 'child', 'teen', 'young', 'adult', 'senior']
+Title_dict = {
+    "Capt": "Officer",
+    "Col": "Officer",
+    "Major": "Officer",
+    "Jonkheer": "Royalty",
+    "Don": "Royalty",
+    "Sir": "Royalty",
+    "Dr": "Officer",
+    "Rev": "Officer",
+    "the Countess": "Royalty",
+    "Mme": "Mrs",
+    "Mlle": "Miss",
+    "Mr": "Mr",
+    "Ms": "Mrs",
+    "Mrs": "Mrs",
+    "Miss": "Miss",
+    "Master": "Master",
+    "Lady": "Royalty"
+}
+
+
+def cleanName(name):
+    w1 = name.split(",")[1]
+    w2 = w1.split(".")[0].strip()
+    return w2
 
 
 def cleanTitanic(df, dropCols,  catClass, medianCols=[]):
@@ -9,6 +34,8 @@ def cleanTitanic(df, dropCols,  catClass, medianCols=[]):
     change to type category and fill few NaN values'''
     df['Age'].fillna(-0.5, inplace=True)
     df['Age'] = pd.cut(df['Age'], ages, labels=labels)
+    df['Name'] = df['Name'].apply(lambda x: cleanName(x))
+    df['Name'] = df.Name.map(Title_dict)
     df['SibSp'] = df['SibSp'].apply(lambda x: 2 if x > 1 else x)
     df['Parch'] = df['Parch'].apply(lambda x: 2 if x > 1 else x)
     df = df.drop(dropCols, axis=1)
--- src/demo.py
+++ src/demo.py
@@ -1,22 +1,25 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
-
-from cleaning import cleanTitanic
 from CreateXandY import XandY
+from cleaning import cleanTitanic
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+pd.options.display.max_columns = 100
+
 
 test = pd.read_csv('test.csv')
 train = pd.read_csv('train.csv')
 
-dropCols = ['Name', 'Ticket', 'Cabin', 'SibSp', 'Parch']
+dropCols = ['Ticket', 'Cabin', ]
 
-catClass = ['Pclass', 'Sex', 'Embarked', 'Age']
+catClass = ['Pclass', 'Sex', 'Embarked', 'Age', 'Name', 'SibSp', 'Parch']
 
 alive = train[train['Survived'] == 1]
 deads = train[train['Survived'] == 0]
@@ -30,12 +33,23 @@ deads = train[train['Survived'] == 0]
 # plt.show()
 
 testCleaned = cleanTitanic(test, dropCols, catClass, ['Fare'])
-trainCleaned = cleanTitanic(train, dropCols, catClass=catClass)
+trainCleaned = cleanTitanic(
+    train, dropCols, catClass=catClass, medianCols=['Fare'])
+
+# print(trainCleaned.head())
+#sns.catplot(kind='bar', data=trainCleaned, x='Name', y='Survived', hue='Sex')
+# plt.show()
+testCleaned[testCleaned.Name.isnull()]['Name'] = 'Dona'
+
+print(testCleaned[testCleaned['PassengerId'] == 1306])
 
 X, y = XandY(trainCleaned, dummyCols=catClass)
-print(X.Fare.head())
 testCleaned['Survived'] = [None]*testCleaned.shape[0]
 Xresult, yresult = XandY(testCleaned, dummyCols=catClass)
+Xresult['Name_Royalty'] = [0]*Xresult.shape[0]
+
+print("Formato de X ", X.head())
+print("Formato de Xresult ", Xresult.head())
 
 Xtrain, Xtest, ytrain, ytest = train_test_split(
     X, y, test_size=.2, random_state=42)
@@ -50,13 +64,22 @@ grid_rf.fit(Xtrain, ytrain)
 model = grid_rf.best_estimator_
 print(grid_rf.best_score_)
 print(grid_rf.best_params_)
+
 ypred = model.predict(Xtest)
 
 print("Accuracy with non seen data: ", accuracy_score(ypred, ytest))
-#scores = cross_val_score(model, X, y, cv=5)
 
-#print("Score with cross validation: ", scores.mean())
+scores = cross_val_score(model, X, y, cv=5)
+print("Score with cross validation: ", scores.mean())
 
 test['Survived'] = model.predict(Xresult)
 
-test[['PassengerId', "Survived"]].to_csv('kaggle_submission4.csv', index=False)
+test[['PassengerId', "Survived"]].to_csv('kaggle_submission5.csv', index=False)
+
+features = pd.DataFrame()
+features['features'] = X.columns
+features['importance'] = model.feature_importances_
+features.sort_values('importance', inplace=True, ascending=False)
+sns.catplot(data=features, kind='bar', y='features',
+            x='importance', orient='h')
+plt.show()
